34edf757da257eae30b4f14caf373cda225847ed
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-09-25 17:43:19 -0300

    05-Transition group
    05-Transition group

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
   <app-header></app-header>
       <div class="container">
         <!-- <One/> -->
-        <Two/>
+        <!-- <Two/> -->
+        <Three/>
       </div>
 </template>
 
 <script setup>
   // import One from './components/transitions/one.vue';
-  import Two from './components/transitions/two.vue'
+  // import Two from './components/transitions/two.vue'
+  import Three from './components/transitions/three.vue';
 </script>
